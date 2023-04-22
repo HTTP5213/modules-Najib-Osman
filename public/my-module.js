4f309885c330a00e8147a36c14c3e6e4a36c2bc9
Najib <njosman2@gmail.com>
2023-04-21 21:16:14 -0400

    second commit

--- public/my-module.js
+++ public/my-module.js
@@ -1,7 +1,7 @@
-import { theShire, buckland } 
+import { theShire, buckland }
 from './another-module.js';
 
-let statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 
 export {
     statement, theShire,
